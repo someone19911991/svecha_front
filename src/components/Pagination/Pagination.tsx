567c752c2b9cb2a4816c0e7a37ec61e355a5049d
Garik <garik.melqonyan1991@gmail.com>
2024-04-24 20:58:24 +0300

    pagination fixed

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -16,19 +16,59 @@ const Pagination: FC<IPaginationProps> = ({productsCount, pageItemsCount, active
     activePage = paginationActivePage[location.pathname] || activePage
     const pagesCount = Math.ceil(productsCount / pageItemsCount)
     const maxFollowingNumbers = 3
+    const maxPaginationItemsCount = 6
 
     useEffect(() => {
-        if(pagesCount <= 5){
-            const arr = Array(pagesCount).fill(null).map((u, i) => `${i + 1}`)
+        // if(pagesCount <= 5){
+        //     const arr = Array(pagesCount).fill(null).map((u, i) => `${i + 1}`)
+        //     setPagesArr(arr)
+        // }
+        // else if(activePage <= pagesCount && activePage >= pagesCount - maxFollowingNumbers){
+        //     setPagesArr(['1', '...', `${pagesCount - 3}`, `${pagesCount - 2}`, `${pagesCount - 1}`, `${pagesCount}`])
+        // }else if(activePage >= 1 && activePage <= 1 + maxFollowingNumbers){
+        //     setPagesArr(['1', '2', '3', '4', '...', `${pagesCount}`])
+        // }else{
+        //     setPagesArr(['1', '....', `${activePage - 1}`, `${activePage}`,  '...', `${pagesCount}`])
+        // }
+
+        if(!pagesCount) return
+
+        if(!pagesArr.length || activePage === 1){
+            const arrToSet = ['1', '2', '3', '4', '...', `${pagesCount}`]
+            setPagesArr(arrToSet)
+        }else if(activePage === pagesCount){
+            const arrToSet = ['1', '...', `${pagesCount - 3}`, `${pagesCount - 2}`, `${pagesCount - 1}`, `${pagesCount}`]
+            setPagesArr(arrToSet)
+        }else if(pagesCount <= maxPaginationItemsCount){
+            const arr = Array(pagesCount).fill(null).map((item, index) => `${index + 1}`)
             setPagesArr(arr)
-        }
-        else if(activePage <= pagesCount && activePage >= pagesCount - maxFollowingNumbers){
-            setPagesArr(['1', '...', `${pagesCount - 3}`, `${pagesCount - 2}`, `${pagesCount - 1}`, `${pagesCount}`])
-        }else if(activePage >= 1 && activePage <= 1 + maxFollowingNumbers){
-            setPagesArr(['1', '2', '3', '4', '...', `${pagesCount}`])
         }else{
-            setPagesArr(['1', '....', `${activePage - 1}`, `${activePage}`,  '...', `${pagesCount}`])
+            let first = pagesArr.slice(0, 2)
+            let middle = pagesArr.slice(2, 4)
+            let last = pagesArr.slice(4)
+            if(`${activePage}` === middle[1]){
+                middle = middle.map(item => `${+item + 1}`)
+                if(+middle[1] === +(last[1]) - 1){
+                    middle = middle.map(item => `${+item - 1}`)
+                    last = [`${+last[1] - 1}`, last[1]]
+                }
+                if(middle[0] > first[1] + 1){
+                    first = [first[0], '...']
+                }
+            }
+            else if(`${activePage}` === middle[0] && first[1] === '...'){
+                middle = middle.map(item => `${+item - 1}`)
+                if(+first[0] + 1 === +middle[0]){
+                    first = [first[0], `${+first[0] + 1}`]
+                    middle = middle.map(item => `${+item + 1}`)
+                }
+                if(last[0] !== '...' && +middle[1] + 1 < +last[0]){
+                    last = ['...', last[1]]
+                }
+            }
+            setPagesArr([...first, ...middle, ...last])
         }
+
     }, [activePage, pagesCount, maxFollowingNumbers])
 
 
@@ -52,7 +92,7 @@ const Pagination: FC<IPaginationProps> = ({productsCount, pageItemsCount, active
     return (
         <ul className={styles.pagination}>
             <li className={activePage === 1 ? styles.inactive : ''} onClick={() => handleActiveChange('prev')}><ImArrowLeft /></li>
-            {pagesArr.map(page => <li className={page === `${activePage}` ? styles.active_page : ''} key={page} onClick={() => handleActiveChange(page)}>{page}</li>)}
+            {pagesArr.map(page => <li className={page === `${activePage}` ? styles.active_page : ''} key={page === '...' ? Math.random() : page} onClick={() => handleActiveChange(page)}>{page}</li>)}
             <li className={activePage === pagesCount ? styles.inactive : ''} onClick={() => handleActiveChange('next')}><ImArrowRight /></li>
         </ul>
     )
